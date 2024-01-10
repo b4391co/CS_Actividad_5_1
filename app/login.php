51556927c1d426c7489c97efe3a772dbe88d3734
b4391co <b4391co@proton.me>
2024-01-10 18:04:58 +0100

    bootstrap

--- app/login.php
+++ app/login.php
@@ -6,7 +6,7 @@
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-<body>
+<body class="position-absolute top-50 start-50 translate-middle">
     <h1 class="display-1">Login</h1>
     <?php
     require_once 'conexion.php';
